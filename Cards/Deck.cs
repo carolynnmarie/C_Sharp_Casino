3589bd6424ad5f383d57e4d7abfb02311ec76a67
Carolynn Vansant <carolynnmarie@gmail.com>
2019-06-14 17:02:46 -0400

    worked on GoFish

--- Cards/Deck.cs
+++ Cards/Deck.cs
@@ -5,75 +5,75 @@ namespace BlackJack.Cards{
 
     public class Deck {
 
-        List<Card> deck {get;set;}
+        List<Card> cards {get;set;}
 
         public Deck(){
-            this.deck = new List<Card>();
+            this.cards = new List<Card>();
             int[] ranks = {1,2,3,4,5,6,7,8,9,10,11,12,13};
             int[] suits = {0,1,2,3};
             foreach(int rankValue in ranks){
                 Rank rank = (Rank) rankValue;
                 foreach(int suitValue in suits){
                     Suit suit = (Suit) suitValue;
-                    deck.Add(new Card((Rank)rank, (Suit)suit));                                      
+                    cards.Add(new Card((Rank)rank, (Suit)suit));                                      
                 }
             }
         }
 
-        public Deck(List<Card> deck){
-            this.deck = deck;
+        public Deck(List<Card> cards){
+            this.cards = cards;
         }
 
         public void Shuffle(){
-            int size = deck.Count;
+            int size = cards.Count;
             Random random = new Random();
             for(int i = 0; i<size; i++){
-                Card card1 = deck[i];
+                Card card1 = cards[i];
                 int x = random.Next(i,size);
-                Card card2 = deck[x];
-                deck[i] = card2;
-                deck[x] = card1;
+                Card card2 = cards[x];
+                cards[i] = card2;
+                cards[x] = card1;
             }
         }
 
         public Card DrawCard(){
-            Card card = deck[deck.Count-1];
-            deck.Remove(card);
+            Card card = cards[cards.Count-1];
+            cards.Remove(card);
             return card;
         }
 
         public List<Card> DealCards(int numDealt){
             List<Card> list = new List<Card>();
             for(int i = 0; i<numDealt; i++){
-                list.Add(deck[deck.Count-1-i]);
+                list.Add(cards[cards.Count-1-i]);
             }
             foreach(Card card in list){
-                deck.Remove(card);
+                cards.Remove(card);
             }
             return list;
         }
 
         public bool RemoveCard(Card card){
             bool remCard = false;
-            if(deck.Contains(card)){
-                deck.Remove(card);
+            if(cards.Contains(card)){
+                cards.Remove(card);
                 remCard = true;
             }
             return remCard;
         }
 
         public void ClearDeck(){
-            deck.Clear();
+            cards.Clear();
         }
 
         public void AddCard(Card card){
-            if(!deck.Contains(card)){
-                deck.Add(card);
+            if(!cards.Contains(card)){
+                cards.Add(card);
             }
         }
 
         public bool ContainsCard(Card card){
-            if(deck.Contains(card)){
+            if(cards.Contains(card)){
                 return true;
             }
             return false;
@@ -81,7 +81,7 @@ namespace BlackJack.Cards{
 
         public string PrintDeck(){
             List<string> deckString = new List<string>();
-            foreach(Card card in deck){
+            foreach(Card card in cards){
                 string x = card.ToString();
                 deckString.Add(x);
             }
@@ -89,7 +89,7 @@ namespace BlackJack.Cards{
         }
 
         // public int DeckSize(){
-        //     return this.deck.Size();
+        //     return this.cards.Size();
         // }
 
         
--- Games/GoFishGame.cs
+++ Games/GoFishGame.cs
@@ -12,24 +12,52 @@ namespace BlackJack.Games{
         public List<Card> playerHand {get;set;}
         public List<Card> dealerHand {get;set;}
         public Deck deck {get;set;}
+        public int playerBooks {get;set;}
+        public int dealerBooks {get;set;}
 
         public GoFishGame(Person player){
             this.player = player;
             this.playerHand = new List<Card>();
             this.dealerHand = new List<Card>();
             this.deck = new Deck();
+            this.playerBooks = 0;
+            this.dealerBooks = 0;
         }
 
         public void Start(){
-
+            deck.Shuffle();
+            playerHand = deck.DealCards(7);
+            dealerHand = deck.DealCards(7);
+            Engine();
+            End();
         }
 
         public void Engine(){
-
+            bool keepPlaying = true;
+            do{
+                PlayerTurn();
+                keepPlaying = CheckSizes();
+                DealerTurn();
+                keepPlaying = CheckSizes();
+            } while (keepPlaying);            
         }
 
         public void PlayerTurn(){
-
+            string currentHand = "";
+            int answer = 0;
+            do{
+                foreach (Card card in playerHand){
+                    currentHand += card.ToString() + " ";
+                }
+                Console.WriteLine("Your hand: " + currentHand + "\nWhat rank do you want to ask for?");
+                bool x = Int32.TryParse(Console.ReadLine(),out answer);
+                if(x){
+                //*Enter game logic here*
+                } else {
+                    Console.WriteLine("Invalid input.")
+                }
+            } while(true);
+            
         }
 
         public void DealerTurn(){
@@ -40,8 +68,16 @@ namespace BlackJack.Games{
             return hand;
         }
 
+        private bool CheckSizes(){
+            if(playerHand.Count == 0 || dealerHand.Count == 0 || deck.cards.Count == 0){
+                return false;
+            }
+            return true;
+        }
+
         public void End(){
 
         }
+
     }
 }
